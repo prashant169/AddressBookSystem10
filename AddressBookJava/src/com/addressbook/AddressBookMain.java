fb6767de1c087a12e52127ca0372013fe22d51a5
prashant169 <prashantphad2018@gmail.com>
2021-10-11 23:10:19 -0800

    Add: Program for create contact in address book

---
+++ AddressBookJava/src/com/addressbook/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.addressbook;
+
+public class AddressBookMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome into the Address Book System in Stream!!!!");
+		AddressBook addressBook = new AddressBook();
+	}
+}
---
+++ AddressBookJava/src/module-info.java
@@ -0,0 +1,2 @@
+module AddressBookJava {
+}
